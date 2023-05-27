88419ad60661d0eda02b62239149f930abdac987
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-27 17:08:01 -0300

    feat: ensure that bcrypt is working properly

---
+++ backend/src/application/repositories/bcrypt/bcrypt-adapter.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt'
+import { Hasher } from '../hash'
+
+export class BcryptAdapter implements Hasher {
+  constructor(private readonly salt: number) {}
+
+  async hash(plaintext: string): Promise<string> {
+    return bcrypt.hash(plaintext, this.salt)
+  }
+
+  async compare(plaintext: string, digest: string): Promise<boolean> {
+    return bcrypt.compare(plaintext, digest)
+  }
+}
