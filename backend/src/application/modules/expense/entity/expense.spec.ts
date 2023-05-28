82ecd11d342b13569ca907af2d69bb7ac3aaaa0d
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-28 11:34:18 -0300

    test ensure that Expense not has 0 balance and future dates

--- backend/src/application/modules/expense/entity/expense.spec.ts
+++ backend/src/application/modules/expense/entity/expense.spec.ts
@@ -2,6 +2,8 @@ import { describe, expect, it } from 'vitest'
 import { makeExpense } from '../tests/factories'
 import { Expense } from './expense'
 import { InvalidCredentialsError } from '../../user/errors/invalid-credentials-error'
+import { InvalidCostError } from '../../user/errors/invalid-cost-error'
+import { AppError } from '@/src/shared/errors/global-errors'
 
 interface SutTypes {
   sut: Expense
@@ -26,5 +28,21 @@ describe('Expense entity', () => {
         description: 'a'.repeat(192),
       })
     }).toThrow(InvalidCredentialsError)
+  })
+
+  it('Should throw InvalidCostError if cost is lower than min cost allowed', () => {
+    expect(() => {
+      makeExpense({
+        cost: 0,
+      })
+    }).toThrow(InvalidCostError)
+  })
+
+  it('Should throw AppError if cost is lower than min cost allowed', () => {
+    expect(() => {
+      makeExpense({
+        createdAt: new Date(new Date().setDate(new Date().getDate() + 1)),
+      })
+    }).toThrow(AppError)
   })
 })
