2415ae183017b6d7f31d007ab423b04d4fad3460
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-28 14:26:25 -0300

    test: ensure that UserRepository will be called with correct values

---
+++ backend/src/application/modules/expense/use-cases/find-expense-use-case.spec.ts
@@ -0,0 +1,43 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { InMemoryExpenseRepository } from '@/src/application/repositories/in-memory/in-memory-expense-repository'
+import { InMemoryUserRepository } from '@/src/application/repositories/in-memory/in-memory-user-repository'
+import { FindExpenseUseCase } from './find-expense-use-case'
+
+import { makeExpense } from '../tests/factories'
+import { NotFoundError } from '@/src/shared/errors/global-errors'
+import { makeUser } from '../../user/tests/factories'
+
+let sut: FindExpenseUseCase
+let inMemoryUserRepository: InMemoryUserRepository
+let inMemoryExpenseRepository: InMemoryExpenseRepository
+
+describe('FindExpenseUseCase', () => {
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository()
+    inMemoryExpenseRepository = new InMemoryExpenseRepository()
+    sut = new FindExpenseUseCase(
+      inMemoryExpenseRepository,
+      inMemoryUserRepository,
+    )
+  })
+
+  it('Should to throw if has not valid payer', () => {
+    expect(async () => {
+      const { payerId, id } = makeExpense()
+      await sut.execute({ payerId, expenseId: id })
+    }).rejects.toBeInstanceOf(NotFoundError)
+  })
+
+  it('Should call UserRepository with correct payerId', async () => {
+    const findByIdSpy = vi.spyOn(inMemoryUserRepository, 'findById')
+    const user = makeUser()
+    await inMemoryUserRepository.add(user)
+
+    const { payerId, id } = makeExpense({
+      payerId: user.id,
+    })
+    await sut.execute({ payerId, expenseId: id })
+
+    expect(findByIdSpy).toHaveBeenCalledWith(payerId)
+  })
+})
