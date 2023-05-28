91eaba749f4c86b169df522ca95b1fa192b87cde
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-28 14:15:45 -0300

    feart: add FetchExpensesController route

--- backend/src/application/http/routes/expense.routes.ts
+++ backend/src/application/http/routes/expense.routes.ts
@@ -2,10 +2,13 @@ import { Router } from 'express'
 import { ExpendController } from '../controllers/expenses/expend-controller'
 import { validateExpenseBody } from '../middlewares/validate-expenses-body'
 import { Authorization } from '../middlewares/ensure-auth'
+import { FetchExpensesController } from '../controllers/expenses/fetch-controller'
 
 const expenseRoute = Router()
-const expendController = new ExpendController()
 const authorization = new Authorization()
+
+const expendController = new ExpendController()
+const fetchExpensesController = new FetchExpensesController()
 
 // protected routes
 expenseRoute.post(
@@ -14,5 +17,6 @@ expenseRoute.post(
   validateExpenseBody,
   expendController.handle,
 )
+expenseRoute.get('/', authorization.ensureAuth, fetchExpensesController.handle)
 
 export { expenseRoute }
