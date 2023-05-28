8bafb19a7b988c0dec1e6bb1eb392592485a9341
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-28 14:26:21 -0300

    feat: ensure that UserRepository will be called with correct values

---
+++ backend/src/application/modules/expense/use-cases/find-expense-use-case.ts
@@ -0,0 +1,25 @@
+import { ExpenseRepository } from '@/src/application/repositories/expense-repository'
+import { UserRepository } from '@/src/application/repositories/user-repository'
+import { NotFoundError } from '@/src/shared/errors/global-errors'
+
+interface FindExpenseUseCaseProps {
+  expenseId: string
+  payerId: string
+}
+
+interface FindExpenseUseCaseResponse {}
+
+export class FindExpenseUseCase {
+  constructor(
+    private readonly expenseRepository: ExpenseRepository,
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async execute({
+    expenseId,
+    payerId,
+  }: FindExpenseUseCaseProps): Promise<FindExpenseUseCaseResponse> {
+    const payer = await this.userRepository.findById(payerId)
+    if (!payer) throw new NotFoundError('Payer not found')
+  }
+}
