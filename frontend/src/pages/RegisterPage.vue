9713a8dff82fd698074e8ee6acc5e9ece71a6d48
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-05-30 00:05:09 -0300

    feat: remove onReset function

--- frontend/src/pages/RegisterPage.vue
+++ frontend/src/pages/RegisterPage.vue
@@ -3,53 +3,23 @@
     <button @click="router.push('/')">
       Voltar
     </button>
-    <q-form
-      @submit.prevent="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md"
-    >
-    <q-input
-        filled
-        v-model="username"
-        label="Your username *"
-        lazy-rules
-        :rules="[
-          val => val !== null && val !== '' || 'Please type your username',
-        ]"
-      />
-    <q-input
-        filled
-        v-model="name"
-        label="Your name *"
-        lazy-rules
-        :rules="[
-          val => val !== null && val !== '' || 'Please type your name',
-        ]"
-      />
-      <q-input
-        filled
-        v-model="password"
-        type="password"
-        label="Your password *"
-        lazy-rules
-        :rules="[
-          val => val !== null && val !== '' || 'Please type your password',
-        ]"
-      />
-      <q-input
-        filled
-        v-model="confirmPassword"
-        type="password"
-        label="Your password *"
-        lazy-rules
-        :rules="[
-          val => val !== null && val !== '' || 'Please type your confirm password',
-          val => val === password || 'Passwords doest not match',
-        ]"
-      />
+    <q-form @submit.prevent="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input filled v-model="username" label="Your username *" lazy-rules :rules="[
+        val => val !== null && val !== '' || 'Please type your username',
+      ]" />
+      <q-input filled v-model="name" label="Your name *" lazy-rules :rules="[
+        val => val !== null && val !== '' || 'Please type your name',
+      ]" />
+      <q-input filled v-model="password" type="password" label="Your password *" lazy-rules :rules="[
+        val => val !== null && val !== '' || 'Please type your password',
+      ]" />
+      <q-input filled v-model="confirmPassword" type="password" label="Your password *" lazy-rules :rules="[
+        val => val !== null && val !== '' || 'Please type your confirm password',
+        val => val === password || 'Passwords doest not match',
+      ]" />
 
       <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Submit" type="submit" color="primary" />
       </div>
     </q-form>
   </div>
@@ -59,29 +29,22 @@
 import { ref } from 'vue'
 import { addUserService } from '../services/add-user-service'
 import { useRouter } from 'vue-router';
-  const router = useRouter()
+const router = useRouter()
 
-  const name = ref(null)
-  const username = ref(null)
-  const password = ref(null)
-  const confirmPassword = ref(null)
+const name = ref(null)
+const username = ref(null)
+const password = ref(null)
+const confirmPassword = ref(null)
 
 
-  async function onSubmit () {
-    // await addUserService({
-    //   name: name.value,
-    //   username: username.value,
-    //   password: password.value,
-    //   confirmPassword: confirmPassword.value
-    // })
+async function onSubmit() {
+  await addUserService({
+    name: name.value,
+    username: username.value,
+    password: password.value,
+    confirmPassword: confirmPassword.value
+  })
+  router.push('/')
+}
 
-    router.push('/')
-  }
-
-  function onReset () {
-    name.value = null
-    username.value = null
-    password.value = null
-    confirmPassword.value = null
-  }
 </script>
